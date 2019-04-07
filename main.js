d926e2a39673faaac4700e5c0c9b68b2a37e6671
Robert <rbalchunas13@gmail.com>
2019-04-07 10:58:00 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let startWindow;
+let streamWindow;
+let settingsWindows;
+
+app.on('ready', function() {
+    startWindow = new BrowserWindow({});
+
+    startWindow.loadFile('startWindow.html');
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Connect'
+            },
+            {
+                label: 'Quit',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
