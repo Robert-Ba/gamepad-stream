37e4779c333a31bae234f4e9185edcadf2821809
Robert <robert.balchunas@maine.edu>
2019-04-08 15:09:59 -0400

    Added various windows and updated UI. Code for fetching the stream has been added. Need to work on starting the server for broadcasting the stream.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const net = require('net');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let startWindow;
 let streamWindow;
@@ -19,29 +21,97 @@ app.on('ready', function() {
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     Menu.setApplicationMenu(mainMenu);
 
+
 });
+
+// Start streaming
+function createStreamViewerWindow() {
+    startWindow.loadFile('streamWindow.html');
+}
+
+function createBroadcastStreamWindow() {
+    startWindow.loadFile('broadcastWindow.html');
+}
+
+function createMainWindow() {
+    startWindow.loadFile('startWindow.html');
+}
+
+
+
+ipcMain.on('stream:join', function (e, ip) {
+    //mainWindow.webContents.send('item:add', item);
+    createStreamViewerWindow();
+});
+
+ipcMain.on('stream:start', function (e) {
+    //mainWindow.webContents.send('item:add', item);
+    createBroadcastStreamWindow();
+    startWindow.webContents.once("did-finish-load", function () {
+        var http = require("http");
+        var crypto = require("crypto");
+        var server = http.createServer(function (req, res) {
+            var port = crypto.randomBytes(16).toString("hex");
+            ipcMain.once(port, function (ev, status, head, body) {
+                res.writeHead(status, head);
+                res.end(body);
+            });
+            window.webContents.send("request", req, port);
+        });
+        server.listen(8000);
+        console.log("http://localhost:8000/");
+    });
+});
+
+ipcMain.on('stream:stop', function (e) {
+    //mainWindow.webContents.send('item:add', item);
+
+    // Stop listening on port.
+
+    createMainWindow();
+});
+
+startWindow.on('closed', function () {
+    startWindow = null;
+})
+
+// TODO
+net.createServer().listen();
+
 
 const mainMenuTemplate = [
     {
         label: 'File',
         submenu: [
             {
-                label: 'Connect'
-            },
-            {
                 label: 'Quit',
                 accelerator: process.platform === "darwin" ? 'Command+Q' : 'Ctrl+Q',
                 click() {
                     app.quit();
                 }
-            },
-            {
-                label: 'Dev Tools',
-                accelerator: process.platform === "darwin" ? 'Command+D' : 'Ctrl+D',
-                click() {
-                    startWindow.webContents.openDevTools()
-                }
             }
         ]
     }
 ];
+
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: "Developer Tools",
+        submenu: [
+            {
+                label: 'Toggle Dev Tools',
+                accelerator: process.platform === "darwin" ? 'Command+D' : 'Ctrl+D',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
